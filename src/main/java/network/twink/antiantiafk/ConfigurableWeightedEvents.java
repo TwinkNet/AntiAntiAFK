a8d143c8b63390eb5d79a232c6e506e26c7ac834
TwinkNet <248537975+TwinkNet@users.noreply.github.com>
2025-12-17 15:37:52 -0500

    Begin writing config.yml

--- src/main/java/network/twink/antiantiafk/ConfigurableWeightedEvents.java
+++ src/main/java/network/twink/antiantiafk/ConfigurableWeightedEvents.java
@@ -1,6 +1,20 @@
 package network.twink.antiantiafk;
 
+import org.bukkit.configuration.file.FileConfiguration;
+
 public class ConfigurableWeightedEvents {
+
+    public static final String EVENTS_PREFIX = "events.";
+    public static final String onNewChunkGeneratedYamlVal = "new-chunk-generated"; // inherently unique
+    public static final String oldChunkLoadedYamlVal = "old-chunk-loaded"; // only a unique chunk for this session
+    public static final String onBlockPlaceYamlVal = "block-placed"; // only in a unique Location
+    public static final String onBlockBreakYamlVal = "block-broken"; // only in a unique Location
+    public static final String onPortalYamlVal = "dimension-changed"; // only in a unique Location
+    public static final String onContainerOpenYamlVal = "container-opened"; // only in a unique Location
+    public static final String onElytraEngageYamlVal = "elytra-used"; // only in a unique Location
+    public static final String onDeathYamlVal = "player-died"; // only in a unique Location
+    public static final String onChatYamlVal = "player-chatted";
+    public static final String onInteractYamlVal = "player-right-click"; // only in a unique Location
 
     private float onNewChunkGenerated; // inherently unique
     private float oldChunkLoaded; // only a unique chunk for this session
@@ -13,9 +27,22 @@ public class ConfigurableWeightedEvents {
     private float onChat;
     private float onInteract; // only in a unique Location
 
+    private final FileConfiguration config;
+
 
     public ConfigurableWeightedEvents(AntiAntiAfkPlugin plugin) {
-        // init config.yml
+        plugin.saveResource("config.yml", false);
+        config = plugin.getConfig();
+        this.onNewChunkGenerated = (float) config.getDouble(EVENTS_PREFIX + onNewChunkGeneratedYamlVal);
+        this.oldChunkLoaded = (float) config.getDouble(EVENTS_PREFIX + oldChunkLoadedYamlVal);
+        this.onBlockPlace = (float) config.getDouble(EVENTS_PREFIX + onBlockPlaceYamlVal);
+        this.onBlockBreak = (float) config.getDouble(EVENTS_PREFIX + onBlockBreakYamlVal);
+        this.onPortal = (float) config.getDouble(EVENTS_PREFIX + onPortalYamlVal);
+        this.onContainerOpen = (float) config.getDouble(EVENTS_PREFIX + onContainerOpenYamlVal);
+        this.onElytraEngage = (float) config.getDouble(EVENTS_PREFIX + onElytraEngageYamlVal);
+        this.onDeath = (float) config.getDouble(EVENTS_PREFIX + onDeathYamlVal);
+        this.onChat = (float) config.getDouble(EVENTS_PREFIX + onChatYamlVal);
+        this.onInteract = (float) config.getDouble(EVENTS_PREFIX + onInteractYamlVal);
     }
 
     public float getOnNewChunkGenerated() {
